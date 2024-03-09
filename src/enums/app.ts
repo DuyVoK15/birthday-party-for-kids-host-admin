ca2df1d929c58e21a492026bb35984fadb608bbc
DuyVoK15 <duybpz@gmail.com>
2024-03-10 02:00:24 +0700

    feat: set up enum

--- src/enums/app.ts
+++ src/enums/app.ts
@@ -1,7 +1,8 @@
 enum AppConstants {
     ACCESS_TOKEN = 'access_token',
     REFRESH_TOKEN = 'refresh_token',
-    USER = "user_information"
+    USER = "user_information",
+    ROLE = "role"
   }
   
   export default AppConstants;
--- src/enums/roles.ts
+++ src/enums/roles.ts
@@ -1,5 +1,5 @@
 export enum ROLE_ENUM {
-    CUSTOMER = 1,
-    HOST_PARTY = 2,
-    ADMIN = 3
+  CUSTOMER = 'CUSTOMER',
+  HOST = 'HOST',
+  ADMIN = 'ADMIN'
 }
