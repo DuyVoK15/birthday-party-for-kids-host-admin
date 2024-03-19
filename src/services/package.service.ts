3428f214ebb1ef02ec94d3a744dd631b0b88baa4
DuyVoK15 <duybpz@gmail.com>
2024-03-19 15:54:39 +0700

    feat: package lack

--- src/services/package.service.ts
+++ src/services/package.service.ts
@@ -17,17 +17,18 @@ export const packageService = {
     formData.append('fileImg', payload.fileImage)
     formData.append('packageName', payload.packageName)
     formData.append('packageDescription', payload.packageDescription)
-    formData.append('pricing', payload.pricing)
+    formData.append('percent', JSON.stringify(payload.percent))
+    formData.append(`packageServiceRequests`, JSON.stringify(payload.packageServiceRequests))
 
     return axiosClient.post(url, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
   },
   updatePackage: (request: { id: number; payload: PackageCreateRequest }): Promise<AxiosResponse<any>> => {
     const url = `/api/package/update-package/${request.id}`
     const formData = new FormData()
-    formData.append('fileImg', request.payload.fileImage || null)
+    formData.append('fileImg', request.payload.fileImage)
     formData.append('packageName', request.payload.packageName)
     formData.append('packageDescription', request.payload.packageDescription)
-    formData.append('pricing', request.payload.pricing)
+    formData.append(`packageServiceRequests`, JSON.stringify(request.payload.packageServiceRequests))
 
     return axiosClient.put(url, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
   },
